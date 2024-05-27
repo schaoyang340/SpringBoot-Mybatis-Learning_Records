bb371ab6eb537840e51a3b151fe7307d66fe6d9b
schaoyang340 <schaoyang340@gmail.com>
2024-05-27 18:21:39 +0800

    更新数据插入

--- src/main/java/org/sql/domain/Employees1.java
+++ src/main/java/org/sql/domain/Employees1.java
@@ -4,78 +4,83 @@ import com.baomidou.mybatisplus.annotation.IdType;
 import com.baomidou.mybatisplus.annotation.TableField;
 import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableName;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import org.springframework.format.annotation.DateTimeFormat;
+
 import java.io.Serializable;
 import java.util.Date;
 
 /**
- * 
+ *
  * @TableName employees1
  */
 @TableName(value ="employees1")
 public class Employees1 implements Serializable {
     /**
-     * 
+     *
      */
     @TableId(value = "employee_id", type = IdType.AUTO)
     private Integer employeeId;
 
     /**
-     * 
+     *
      */
     @TableField(value = "first_name")
     private String firstName;
 
     /**
-     * 
+     *
      */
     @TableField(value = "last_name")
     private String lastName;
 
     /**
-     * 
+     *
      */
     @TableField(value = "email")
     private String email;
 
     /**
-     * 
+     *
      */
     @TableField(value = "phone_number")
     private String phoneNumber;
 
     /**
-     * 
+     *
      */
     @TableField(value = "job_id")
     private String jobId;
 
     /**
-     * 
+     *
      */
     @TableField(value = "salary")
     private Double salary;
 
     /**
-     * 
+     *
      */
     @TableField(value = "commission_pct")
     private Double commissionPct;
 
     /**
-     * 
+     *
      */
     @TableField(value = "manager_id")
     private Integer managerId;
 
     /**
-     * 
+     *
      */
     @TableField(value = "department_id")
     private Integer departmentId;
 
     /**
-     * 
+     *
      */
+//    @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "Asia/Shanghai")
     @TableField(value = "hiredate")
     private Date hiredate;
 
